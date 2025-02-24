54c142c416a466d104de089eeb0f529da4f23437
Saksham Jain <sakshambro730@gmail.com>
2025-02-24 19:28:20 +0530

    Done

--- MERN Apps/Digital Diary App/client/vite.config.js
+++ MERN Apps/Digital Diary App/client/vite.config.js
@@ -1,17 +1,16 @@
-import path from 'path'
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, 'src'),
-			'~': path.resolve(__dirname, 'node_modules')
-		}
-	},
-	build: {
-		chunkSizeWarningLimit: 1600
-	}
-})
+import path from 'path'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			'~': path.resolve(__dirname, 'node_modules')
+		}
+	},
+	build: {
+		chunkSizeWarningLimit: 1600
+	}
+});
